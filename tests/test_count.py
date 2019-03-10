31068fd28ca066fb542388ded8b77d5135b3845d
sahil sodhi <sahilsodhi@sahils-MacBook-Pro.local>
2019-03-10 03:51:02 -0400

    Modified test cases.

--- tests/test_count.py
+++ tests/test_count.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, './answers')
 from answer import count
 
+#Assert values from 2016 file only.
 def test_count():
     a = count("./data/frenepublicinjection2016.csv")
     assert(a == 27244)
